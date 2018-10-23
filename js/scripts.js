4a20c291cdd4882aecfd6b8c6f27219aba4e3418
kohichi horii and ryan leslie <ryan@>
2018-10-23 15:09:16 -0700

    create new functions for counting webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,39 @@
-$(function(){
-  $("#numbers").submit(function(event){
-    event.preventDefault();
+function myfunc(topNumber){
+var array = [];
+  for (var index = 0; index <= topNumber; index ++){
+    array.push[index];
+}
 
-
-    var total = 0;
-    for (var startingNumber = 0; startingNumber <= 30; startingNumber += 5){
-
-      if (startingNumber <= 30){
-
-
-        $(".count").append("<li>" + startingNumber + "</li>");
-      }
-    }
-var total = 0
-for ( var newnumber = 0; newnumber<=49; newnumber += 7){
-  if (newnumber <= 49){
-    $(".count2").append("<li>" + newnumber + "</li>");
-  }
+  return topNumber;
 }
 
 
-    
+
+$(function(){
+  $("#numbers").submit(function(event){
+
+    var input = $("#one").val();
+    var answer = myfunc(input)
+    $(".count").append("<li>"  + answer + "</li>");
+
+    event.preventDefault();
+
   });
 });
+
+//
+//     var total = 0;
+//     for (var startingNumber = 0; startingNumber <= 30; startingNumber += 5){
+//
+//       if (startingNumber <= 30){
+//
+//
+//         $(".count").append("<li>" + startingNumber + "</li>");
+//       }
+//     }
+// var total = 0
+// for ( var newnumber = 0; newnumber<=49; newnumber += 7){
+//   if (newnumber <= 49){
+//     $(".count2").append("<li>" + newnumber + "</li>");
+//   }
+// }
